0e40a6a65b0687eccbb94ebe5b2b9749554573c2
azizbrt <157480466+azizbrt@users.noreply.github.com>
2025-04-28 17:54:18 +0100

    connect events page with backend

--- routes/categorieRoutes.js
+++ routes/categorieRoutes.js
@@ -6,7 +6,7 @@ const app = express();
 //create a new categorie
 app.post("/create", verifyToken, verifyRole(["admin"]),createCategory);
 //get
-app.get("/get",verifyToken, verifyRole(["admin","gestionnaire"]),getAllCategories);
+app.get("/get",verifyToken, verifyRole(["admin","gestionnaire","participant"]),getAllCategories);
 //deletze
 app.delete("/delete/:id",verifyToken, verifyRole(["admin"]), deleteCategories);
 //modifay
